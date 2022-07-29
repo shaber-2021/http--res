12c25ccb015db5a56c2b9ba1d8945dd4f81b67cf
shaber <shaberahmad2019s@gmail.com>
2022-07-29 14:17:59 +0300

    this is node routing commit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const http  = require('http');
+const fs = require('fs');
 const port = 3000;
 const hostname = '127.0.0.1'
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(202,{'Content-type':'text/html'})
-    res.write('<h1>my hobbis i am a webdeveloper</h1>');
-    res.end()
+  if(req.url==='/'){
+    fs.readFile('index.html',(err,data)=>{
+        res.writeHead(202,{'Content-type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+  }
+  else if(req.url==='/about'){
+    fs.readFile('about.html',(err,data)=>{
+        res.writeHead(202,{'Content-type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+  }
+  if(req.url==='/contoct'){
+    fs.readFile('contoct.html',(err,data)=>{
+        res.writeHead(202,{'Content-type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+  };;
 });
 
 server.listen(port,hostname,()=>{
